b1c8f06ffb327861fcf7c616b1525b1d290c222b
iheboueslati909 <hooba500@gmail.com>
2025-03-27 01:14:03 +0100

    Created aggregates

---
+++ eventify.Domain/Aggregates/EventAggregate.cs
@@ -0,0 +1,28 @@
+﻿using eventify.Domain.Entities;
+using System.Collections.Generic;
+
+namespace eventify.Domain.Aggregates;
+
+public class EventAggregate
+{
+    public Event Event { get; private set; }
+    public List<BookingInvitation> BookingInvitations { get; private set; } = new();
+    public List<MemberEvent> MemberEvents { get; private set; } = new();
+
+    private EventAggregate() { } // Required for EF Core
+
+    public EventAggregate(Event eventEntity)
+    {
+        Event = eventEntity;
+    }
+
+    public void AddBookingInvitation(BookingInvitation invitation)
+    {
+        BookingInvitations.Add(invitation);
+    }
+
+    public void AddMemberEvent(MemberEvent memberEvent)
+    {
+        MemberEvents.Add(memberEvent);
+    }
+}
---
+++ eventify.Domain/Aggregates/MemberAggregate.cs
@@ -0,0 +1,28 @@
+﻿using eventify.Domain.Entities;
+using System.Collections.Generic;
+
+namespace eventify.Domain.Aggregates;
+
+public class MemberAggregate
+{
+    public Member Member { get; private set; }
+    public List<MemberEvent> MemberEvents { get; private set; } = new();
+    public List<BookingInvitation> BookingInvitations { get; private set; } = new();
+
+    private MemberAggregate() { } // Required for EF Core
+
+    public MemberAggregate(Member member)
+    {
+        Member = member;
+    }
+
+    public void AddMemberEvent(MemberEvent memberEvent)
+    {
+        MemberEvents.Add(memberEvent);
+    }
+
+    public void AddBookingInvitation(BookingInvitation invitation)
+    {
+        BookingInvitations.Add(invitation);
+    }
+}
