88e3f691b1559e45ec9075025a536c73ce3bc67b
iheboueslati909 <hooba500@gmail.com>
2025-04-16 12:25:02 +0100

    create concept unified command and handler in one class

--- eventify.Application/Concepts/CreateConceptCommand.cs
+++
@@ -1,9 +0,0 @@
-namespace eventify.Application.Concepts.Commands;
-
-public class CreateConceptCommand
-{
-    public Guid MemberId { get; set; }
-    public string Title { get; set; } = default!;
-    public string Description { get; set; } = default!;
-    public List<int> Genres { get; set; } = new();
-}
